cf4ecbdc0b6af0a860a5400fbb86a303eb7e4c86
Genert Org <orggenert@gmail.com>
2019-09-12 12:31:15 +0300

    misc

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/genertorg/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/genertorg/git/pusher-websocket-flutter/example"
-export "FLUTTER_TARGET=/Users/genertorg/git/pusher-websocket-flutter/example/lib/main.dart"
+export "FLUTTER_APPLICATION_PATH=/Users/genertorg/indoor.ninja/flutter_pusher/example"
+export "FLUTTER_TARGET=/Users/genertorg/indoor.ninja/flutter_pusher/example/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "FLUTTER_FRAMEWORK_DIR=/Users/genertorg/flutter/bin/cache/artifacts/engine/ios"
